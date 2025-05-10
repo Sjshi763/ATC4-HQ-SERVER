4897260731c3990cdc52cfe3bee446132e26ecb5
圣达生物多 <qq3258819795@163.com>
2025-05-10 00:17:48 +0000

    3-监听端口

--- mian.cpp
+++ mian.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
 namespace master {
+
 }
-int main () {
-    return 0 ;
+int main() {
+    using namespace master;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    // 创建套接字
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    // 设置套接字选项
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+    // 配置地址和端口
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // 监听所有网络接口
+    address.sin_port = htons(1);      // 监听端口 8080
+    // 绑定套接字到地址和端口
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    // 开始监听
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Listening on port 1..." << std::endl;
+    // 接受连接
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "完成连接" << std::endl;
+    // 关闭套接字
+    close(new_socket);
+    close(server_fd);
+    return 0;
 }
